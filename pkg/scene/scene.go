9981e1ed7dbeeddeb152ae88eaf7c92806372a23
Miguel Mendez <miguel@emerald-isle.eu>
2022-05-01 18:13:04 +0100

    Add scene package which simplifies passing scene data around.

---
+++ pkg/scene/scene.go
@@ -0,0 +1,23 @@
+// Package scene implements structures and methods to work with scenes.
+package scene
+
+import (
+	"gitlab.com/flynn-nrg/izpi/pkg/camera"
+	"gitlab.com/flynn-nrg/izpi/pkg/hitable"
+)
+
+// Scene represents a scene with the world elements, lights and camera.
+type Scene struct {
+	World  *hitable.HitableSlice
+	Lights *hitable.HitableSlice
+	Camera *camera.Camera
+}
+
+// New returns a new scene instance.
+func New(world *hitable.HitableSlice, lights *hitable.HitableSlice, camera *camera.Camera) *Scene {
+	return &Scene{
+		World:  world,
+		Lights: lights,
+		Camera: camera,
+	}
+}
