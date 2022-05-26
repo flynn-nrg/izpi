d3649b38e7372c7aae16ebc7b743bfb7ab57f262
Miguel Mendez <miguel@emerald-isle.eu>
2022-05-26 15:31:48 +0100

    Add gamma filter.

---
+++ pkg/postprocess/gamma.go
@@ -0,0 +1,41 @@
+package postprocess
+
+import (
+	"errors"
+	"image"
+	"math"
+
+	"github.com/flynn-nrg/izpi/pkg/colour"
+	"github.com/flynn-nrg/izpi/pkg/floatimage"
+	"github.com/flynn-nrg/izpi/pkg/scene"
+)
+
+// Ensure interface compliance.
+var _ Filter = (*Gamma)(nil)
+
+// Gamma represents a gamma adjustment filter.
+type Gamma struct{}
+
+// NewClamp returns a new clamp filter.
+func NewGamma() *Gamma {
+	return &Gamma{}
+}
+
+func (g *Gamma) Apply(i image.Image, _ *scene.Scene) error {
+	im, ok := i.(*floatimage.FloatNRGBA)
+	if !ok {
+		return errors.New("only FloatNRGBA image format is supported")
+	}
+	for y := i.Bounds().Min.Y; y <= i.Bounds().Max.Y; y++ {
+		for x := i.Bounds().Min.X; x <= i.Bounds().Max.X; x++ {
+			pixel := im.FloatNRGBAAt(x, y)
+			im.Set(x, y, colour.FloatNRGBA{
+				R: math.Sqrt(pixel.R),
+				G: math.Sqrt(pixel.G),
+				B: math.Sqrt(pixel.B),
+				A: pixel.A})
+		}
+	}
+
+	return nil
+}
