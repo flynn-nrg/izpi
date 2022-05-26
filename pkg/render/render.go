c3cd621a39e353b8d538f3240149eaec14f297da
Miguel Mendez <miguel@emerald-isle.eu>
2022-05-26 15:32:51 +0100

    Keep all data in linear colour space.

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -114,9 +114,8 @@ func renderRect(w workUnit) {
 				col = vec3.Add(col, vec3.DeNAN(w.renderFunc(r, w.scene.World, w.scene.Lights, 0)))
 			}
 
+			// Linear colour space.
 			col = vec3.ScalarDiv(col, float64(w.numSamples))
-			// gamma 2
-			col = &vec3.Vec3Impl{X: math.Sqrt(col.X), Y: math.Sqrt(col.Y), Z: math.Sqrt(col.Z)}
 			w.canvas.Set(x, ny-y, colour.FloatNRGBA{R: col.X, G: col.Y, B: col.Z, A: 1.0})
 			if w.preview {
 				tile.Pixels[i] = col.Z
