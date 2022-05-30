eb892921008c5e4d870bd55e1922f8a46919b7e1
Miguel Mendez <miguel@emerald-isle.eu>
2022-05-30 13:30:51 +0100

    Allow for a small delta so that we can we can actually draw something. Add test for a point not part of the segment.

--- pkg/segment/segment.go
+++ pkg/segment/segment.go
@@ -14,8 +14,8 @@ func Belongs(s *Segment, c *vec3.Vec3Impl) bool {
 	ab := vec3.Sub(s.B, s.A)
 	ac := vec3.Sub(c, s.A)
 
-	// Check if they're colinear.
-	if vec3.Cross(ab, ac).Length() != 0 {
+	// Check if they're colinear. We allow a small angle so that we actually draw something.
+	if vec3.Cross(vec3.UnitVector(ab), vec3.UnitVector(ac)).Length() >= .005 {
 		return false
 	}
 
--- pkg/segment/segment_test.go
+++ pkg/segment/segment_test.go
@@ -41,6 +41,14 @@ func TestBelongs(t *testing.T) {
 			p:    &vec3.Vec3Impl{X: 0, Y: .5, Z: 2},
 			want: true,
 		},
+		{
+			name: "Point is not part of this segment",
+			s: &Segment{
+				A: &vec3.Vec3Impl{X: -1, Y: 0, Z: 0},
+				B: &vec3.Vec3Impl{X: 1, Y: 1, Z: 4},
+			},
+			p: &vec3.Vec3Impl{X: 0, Y: .5, Z: 3},
+		},
 	}
 
 	for _, test := range testData {
