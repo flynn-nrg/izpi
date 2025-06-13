31ad05e4e133d4840011c1e1dae7974526b71536
Miguel Mendez <miguel@emerald-isle.eu>
2025-06-13 18:01:41 +0200

    add transport package and temporary config.

---
+++ internal/transport/transport.go
@@ -0,0 +1,191 @@
+package transport
+
+import (
+	"fmt"
+
+	"github.com/flynn-nrg/izpi/internal/camera"
+	"github.com/flynn-nrg/izpi/internal/hitable"
+	"github.com/flynn-nrg/izpi/internal/material"
+	pb_transport "github.com/flynn-nrg/izpi/internal/proto/transport"
+	"github.com/flynn-nrg/izpi/internal/scene"
+	"github.com/flynn-nrg/izpi/internal/vec3"
+)
+
+type Transport struct {
+	aspectOverride float64
+	protoScene     *pb_transport.Scene
+	triangles      []*pb_transport.Triangle
+	textures       map[string][]byte
+	materials      map[string]material.Material
+}
+
+func NewTransport(aspectOverride float64, protoScene *pb_transport.Scene, triangles []*pb_transport.Triangle, textures map[string][]byte) *Transport {
+	return &Transport{aspectOverride: aspectOverride, protoScene: protoScene, triangles: triangles, textures: textures}
+}
+
+func (t *Transport) ToScene() (*scene.Scene, error) {
+	camera := t.toSceneCamera(t.protoScene.GetCamera(), t.aspectOverride)
+
+	hitables, err := t.toSceneObjects()
+	if err != nil {
+		return nil, err
+	}
+
+	lights := []hitable.Hitable{}
+	for _, h := range hitables {
+		if h.IsEmitter() {
+			lights = append(lights, h)
+		}
+	}
+
+	return &scene.Scene{
+		World:  hitable.NewSlice([]hitable.Hitable{hitable.NewBVH(hitables, 0, 1)}),
+		Lights: hitable.NewSlice(lights),
+		Camera: camera,
+	}, nil
+}
+
+func (t *Transport) toSceneCamera(protoCamera *pb_transport.Camera, aspectOverride float64) *camera.Camera {
+	lookFrom := &vec3.Vec3Impl{
+		X: float64(protoCamera.GetLookfrom().GetX()),
+		Y: float64(protoCamera.GetLookfrom().GetY()),
+		Z: float64(protoCamera.GetLookfrom().GetZ()),
+	}
+
+	lookAt := &vec3.Vec3Impl{
+		X: float64(protoCamera.GetLookat().GetX()),
+		Y: float64(protoCamera.GetLookat().GetY()),
+		Z: float64(protoCamera.GetLookat().GetZ()),
+	}
+
+	vup := &vec3.Vec3Impl{
+		X: float64(protoCamera.GetVup().GetX()),
+		Y: float64(protoCamera.GetVup().GetY()),
+		Z: float64(protoCamera.GetVup().GetZ()),
+	}
+
+	vfov := float64(protoCamera.GetVfov())
+
+	var aspect float64
+	if aspectOverride != 0.0 {
+		aspect = aspectOverride
+	} else {
+		aspect = float64(protoCamera.GetAspect())
+	}
+
+	aperture := float64(protoCamera.GetAperture())
+	focusDist := float64(protoCamera.GetFocusdist())
+	time0 := float64(protoCamera.GetTime0())
+	time1 := float64(protoCamera.GetTime1())
+
+	return camera.New(lookFrom, lookAt, vup, vfov, aspect, aperture, focusDist, time0, time1)
+}
+
+func (t *Transport) toSceneObjects() ([]hitable.Hitable, error) {
+	hitables := []hitable.Hitable{}
+
+	triangles, err := t.toSceneTriangles()
+	if err != nil {
+		return nil, err
+	}
+	hitables = append(hitables, triangles...)
+
+	spheres, err := t.toSceneSpheres()
+	if err != nil {
+		return nil, err
+	}
+	hitables = append(hitables, spheres...)
+
+	return hitables, nil
+}
+
+func (t *Transport) toSceneTriangles() ([]hitable.Hitable, error) {
+	hitables := []hitable.Hitable{}
+
+	// Embedded triangles
+	for _, triangle := range t.protoScene.GetObjects().GetTriangles() {
+		tri, err := t.toSceneTriangle(triangle)
+		if err != nil {
+			return nil, err
+		}
+		hitables = append(hitables, tri)
+	}
+
+	// Streamed triangles
+	for _, triangle := range t.triangles {
+		tri, err := t.toSceneTriangle(triangle)
+		if err != nil {
+			return nil, err
+		}
+		hitables = append(hitables, tri)
+	}
+
+	return hitables, nil
+}
+
+func (t *Transport) toSceneTriangle(triangle *pb_transport.Triangle) (*hitable.Triangle, error) {
+	material, ok := t.materials[triangle.GetMaterialName()]
+	if !ok {
+		return nil, fmt.Errorf("material %s not found", triangle.GetMaterialName())
+	}
+
+	vertex0 := &vec3.Vec3Impl{
+		X: float64(triangle.GetVertex0().GetX()),
+		Y: float64(triangle.GetVertex0().GetY()),
+		Z: float64(triangle.GetVertex0().GetZ()),
+	}
+
+	vertex1 := &vec3.Vec3Impl{
+		X: float64(triangle.GetVertex1().GetX()),
+		Y: float64(triangle.GetVertex1().GetY()),
+		Z: float64(triangle.GetVertex1().GetZ()),
+	}
+
+	vertex2 := &vec3.Vec3Impl{
+		X: float64(triangle.GetVertex2().GetX()),
+		Y: float64(triangle.GetVertex2().GetY()),
+		Z: float64(triangle.GetVertex2().GetZ()),
+	}
+
+	u0 := float64(triangle.GetUv0().GetU())
+	v0 := float64(triangle.GetUv0().GetV())
+	u1 := float64(triangle.GetUv1().GetU())
+	v1 := float64(triangle.GetUv1().GetV())
+	u2 := float64(triangle.GetUv2().GetU())
+	v2 := float64(triangle.GetUv2().GetV())
+
+	tri := hitable.NewTriangleWithUV(vertex0, vertex1, vertex2, u0, v0, u1, v1, u2, v2, material)
+
+	return tri, nil
+}
+
+func (t *Transport) toSceneSpheres() ([]hitable.Hitable, error) {
+	hitables := []hitable.Hitable{}
+
+	for _, sphere := range t.protoScene.GetObjects().GetSpheres() {
+		sphere, err := t.toSceneSphere(sphere)
+		if err != nil {
+			return nil, err
+		}
+		hitables = append(hitables, sphere)
+	}
+
+	return hitables, nil
+}
+
+func (t *Transport) toSceneSphere(sphere *pb_transport.Sphere) (*hitable.Sphere, error) {
+	material, ok := t.materials[sphere.GetMaterialName()]
+	if !ok {
+		return nil, fmt.Errorf("material %s not found", sphere.GetMaterialName())
+	}
+
+	center := &vec3.Vec3Impl{
+		X: float64(sphere.GetCenter().GetX()),
+		Y: float64(sphere.GetCenter().GetY()),
+		Z: float64(sphere.GetCenter().GetZ()),
+	}
+
+	radius := float64(sphere.GetRadius())
+
+	return hitable.NewSphere(center, center, 0, 1, radius, material), nil
+}
