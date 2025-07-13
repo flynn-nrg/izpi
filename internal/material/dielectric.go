7f3f3664d36682b26e3216bb89af5909e7eedd42
Miguel Mendez <miguel@emerald-isle.eu>
2025-07-13 17:00:02 +0100

    add support for spectral rendering in the dieletrict material.

--- internal/material/dielectric.go
+++ internal/material/dielectric.go
@@ -5,6 +5,7 @@ import (
 	"github.com/flynn-nrg/izpi/internal/hitrecord"
 	"github.com/flynn-nrg/izpi/internal/ray"
 	"github.com/flynn-nrg/izpi/internal/scatterrecord"
+	"github.com/flynn-nrg/izpi/internal/texture"
 	"github.com/flynn-nrg/izpi/internal/vec3"
 )
 
@@ -14,9 +15,9 @@ var _ Material = (*Dielectric)(nil)
 // Dielectric represents a dielectric material.
 type Dielectric struct {
 	nonPBR
-	nonSpectral
 	nonEmitter
-	refIdx float64
+	refIdx         float64
+	spectralRefIdx texture.SpectralTexture
 }
 
 // NewDielectric returns an instance of a dielectric material.
@@ -26,8 +27,15 @@ func NewDielectric(reIdx float64) *Dielectric {
 	}
 }
 
-// Scatter computes how the ray bounces off the surface of a dielectric material.
-func (d *Dielectric) Scatter(r ray.Ray, hr *hitrecord.HitRecord, random *fastrandom.LCG) (*ray.RayImpl, *scatterrecord.ScatterRecord, bool) {
+// NewSpectralDielectric returns an instance of a dielectric material with spectral refractive index.
+func NewSpectralDielectric(spectralRefIdx texture.SpectralTexture) *Dielectric {
+	return &Dielectric{
+		spectralRefIdx: spectralRefIdx,
+	}
+}
+
+// scatterCommon contains the common scattering logic for both RGB and spectral rendering
+func (d *Dielectric) scatterCommon(r ray.Ray, hr *hitrecord.HitRecord, random *fastrandom.LCG, refIdx float64) (*ray.RayImpl, bool) {
 	var niOverNt float64
 	var cosine float64
 	var reflectProb float64
@@ -37,20 +45,19 @@ func (d *Dielectric) Scatter(r ray.Ray, hr *hitrecord.HitRecord, random *fastran
 	var outwardNormal *vec3.Vec3Impl
 
 	reflected := reflect(r.Direction(), hr.Normal())
-	attenuation := &vec3.Vec3Impl{X: 1.0, Y: 1.0, Z: 1.0}
 
 	if vec3.Dot(r.Direction(), hr.Normal()) > 0 {
 		outwardNormal = vec3.ScalarMul(hr.Normal(), -1.0)
-		niOverNt = d.refIdx
-		cosine = d.refIdx * vec3.Dot(r.Direction(), hr.Normal()) / r.Direction().Length()
+		niOverNt = refIdx
+		cosine = refIdx * vec3.Dot(r.Direction(), hr.Normal()) / r.Direction().Length()
 	} else {
 		outwardNormal = hr.Normal()
-		niOverNt = 1.0 / d.refIdx
+		niOverNt = 1.0 / refIdx
 		cosine = -vec3.Dot(r.Direction(), hr.Normal()) / r.Direction().Length()
 	}
 
 	if refracted, ok = refract(r.Direction(), outwardNormal, niOverNt); ok {
-		reflectProb = schlick(cosine, d.refIdx)
+		reflectProb = schlick(cosine, refIdx)
 	} else {
 		reflectProb = 1.0
 	}
@@ -60,7 +67,37 @@ func (d *Dielectric) Scatter(r ray.Ray, hr *hitrecord.HitRecord, random *fastran
 	} else {
 		scattered = ray.New(hr.P(), refracted, r.Time())
 	}
+	return scattered, true
+}
+
+// Scatter computes how the ray bounces off the surface of a dielectric material.
+func (d *Dielectric) Scatter(r ray.Ray, hr *hitrecord.HitRecord, random *fastrandom.LCG) (*ray.RayImpl, *scatterrecord.ScatterRecord, bool) {
+	scattered, ok := d.scatterCommon(r, hr, random, d.refIdx)
+	if !ok {
+		return nil, nil, false
+	}
+	attenuation := &vec3.Vec3Impl{X: 1.0, Y: 1.0, Z: 1.0}
 	scatterRecord := scatterrecord.New(scattered, true, attenuation, nil, nil, nil, nil)
+	return scattered, scatterRecord, true
+}
+
+// SpectralScatter computes how the ray bounces off the surface of a dielectric material with spectral properties.
+func (d *Dielectric) SpectralScatter(r ray.Ray, hr *hitrecord.HitRecord, random *fastrandom.LCG) (*ray.RayImpl, *scatterrecord.SpectralScatterRecord, bool) {
+	// For spectral rendering, we need to get the wavelength from the ray
+	// Assuming the ray has a wavelength field or we need to sample one
+	lambda := 550.0 // Default wavelength, should be extracted from ray or sampled
+
+	// Get wavelength-dependent refractive index
+	refIdx := d.spectralRefIdx.Value(hr.U(), hr.V(), lambda, hr.P())
+
+	// Use the common scattering logic with spectral refractive index
+	scattered, ok := d.scatterCommon(r, hr, random, refIdx)
+	if !ok {
+		return nil, nil, false
+	}
+
+	albedo := 1.0 // Dielectrics have no absorption in the visible spectrum
+	scatterRecord := scatterrecord.NewSpectralScatterRecord(scattered, true, albedo, lambda, nil, 0.0, 0.0, nil)
 	return scattered, scatterRecord, true
 }
 
@@ -80,4 +117,10 @@ func (d *Dielectric) Emitted(_ ray.Ray, _ *hitrecord.HitRecord, _ float64, _ flo
 
 func (d *Dielectric) Albedo(u float64, v float64, p *vec3.Vec3Impl) *vec3.Vec3Impl {
 	return &vec3.Vec3Impl{X: 1.0, Y: 1.0, Z: 1.0}
+}
+
+// SpectralAlbedo returns the spectral albedo at the given wavelength.
+// Dielectrics have no absorption in the visible spectrum, so this returns 1.0.
+func (d *Dielectric) SpectralAlbedo(u float64, v float64, lambda float64, p *vec3.Vec3Impl) float64 {
+	return 1.0
 }
